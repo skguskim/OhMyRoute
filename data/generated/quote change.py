4be5636656d00b94afdb1a2fac9aaced978fee94
BotanicalHouse <hyun18273496@gmail.com>
2026-08-11 15:55:59 +0900

    build: 데이터 후처리 파일 생성  - edit sql로 csv 목록에 없는 장소들을 sql에서 삭제  - quote change로 엑셀에서 csv파일 수정 저장 후 큰따옴표 추가

---
+++ data/generated/quote change.py
@@ -0,0 +1,13 @@
+import csv
+import pandas as pd
+from pathlib import Path
+ROOT_DIR = Path(".").resolve()
+GENERATED_DIR = ROOT_DIR / "OhMyRoute" / "data" / "generated"
+
+op_info_df = pd.read_csv(GENERATED_DIR / "gwangju_official_operating_info_review.csv", encoding='utf-8')
+place_profiles_df = pd.read_csv(GENERATED_DIR / "gwangju_official_place_profiles.csv", encoding='utf-8')
+places_df = pd.read_csv(GENERATED_DIR / "gwangju_official_places.csv", encoding='utf-8')
+
+op_info_df.to_csv(GENERATED_DIR / "gwangju_official_operating_info_review.csv", index=False, quoting=csv.QUOTE_ALL, encoding='utf-8')
+place_profiles_df.to_csv(GENERATED_DIR / "gwangju_official_place_profiles.csv", index=False, quoting=csv.QUOTE_ALL, encoding='utf-8')
+places_df.to_csv(GENERATED_DIR / "gwangju_official_places.csv", index=False, quoting=csv.QUOTE_ALL, encoding='utf-8')
